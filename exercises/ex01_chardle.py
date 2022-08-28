8ac31524b7b83914ae33c4bad4de1348ea89de0a
Adesuwa Igbinigie <adesigbi@unc.edu>
2022-08-28 13:50:30 -0400

    got rid of "were" and "was" removed spaces before]

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -10,24 +10,24 @@ if len(chardle_character) != 1:
     print("Error: Character must be a single character.")
     exit()
 print("Searching for " + chardle_character + " in " + chardle_word)
-if chardle_character == chardle_word [0]:
+if chardle_character == chardle_word[0]:
     print(chardle_character + " found at index 0")
     instances_of_character = instances_of_character + 1
-if chardle_character == chardle_word [1]:
+if chardle_character == chardle_word[1]:
     print(chardle_character + " found at index 1")
     instances_of_character = instances_of_character + 1      
-if chardle_character == chardle_word [2]:
+if chardle_character == chardle_word[2]:
     print(chardle_character + " found at index 2") 
     instances_of_character = instances_of_character + 1
-if chardle_character == chardle_word [3]:
+if chardle_character == chardle_word[3]:
     print(chardle_character + " found at index 3") 
     instances_of_character = instances_of_character + 1
-if chardle_character == chardle_word [4]:
+if chardle_character == chardle_word[4]:
     print(chardle_character + " found at index 4") 
     instances_of_character = instances_of_character + 1 
 if instances_of_character == 0:
     print("No instances of " + chardle_character + " found in " + chardle_word)
 if instances_of_character == 1:
-    print(str(instances_of_character) + " instance of " + chardle_character + " was found in " + chardle_word)
+    print(str(instances_of_character) + " instance of " + chardle_character + " found in " + chardle_word)
 if instances_of_character > 1:
-    print(str(instances_of_character) + " instances of " + chardle_character + " were found in " + chardle_word)    
+    print(str(instances_of_character) + " instances of " + chardle_character + " found in " + chardle_word) 
