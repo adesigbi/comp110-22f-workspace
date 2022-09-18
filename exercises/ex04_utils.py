db0692108cc38d518dcbf0262b5aa4c702e7a217
Adesuwa Igbinigie <adesigbi@unc.edu>
2022-09-18 00:42:27 -0400

    Made some changes to all function

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,7 +5,8 @@ __author__ = "730572167"
 
 def all(xs: list[int], x: int) -> bool:
     """Finds if a list is made completly of a given integer."""
-    assert len(xs) != 0
+    if len(xs) == 0:
+        return False
     i: int = 0
     while i < len(xs):
         if xs[i] != x:
